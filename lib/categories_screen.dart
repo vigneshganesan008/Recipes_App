f489c84f39738e6471cbb62e97cdd564cb903872
Vignesh Ganesan <vicky08rockz@gmail.com>
2020-07-18 22:24:03 +0530

    theme data

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,16 +1,25 @@
+import 'package:Meals_App/dummy_data.dart';
+import 'category_item.dart';
 import "package:flutter/material.dart";
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meals App"),
       ),
-      children: <Widget>[],
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItem(e.title, e.color))
+            .toList(),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Meals_App/categories_screen.dart';
 import "package:flutter/material.dart";
 
 main(List<String> args) {
@@ -9,25 +10,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Meals App",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Meals App"),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.purpleAccent,
+        fontFamily: "Raleway",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: TextStyle(fontSize: 20, fontFamily: "RobotoComdensed"),
+            ),
       ),
-      body: Container(),
+      home: CategoriesScreen(),
     );
   }
 }
